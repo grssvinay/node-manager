1c95e51be96fb01c83cd04be5ff60af28ae52204
Vinay Garimella <vinay.garimella@NTPL-ITMACLAP0208.local>
2022-08-08 18:24:52 +0530

    indentation

--- darwin/install.sh
+++ darwin/install.sh
@@ -7,32 +7,32 @@ default_shell=`dscl . -read ~/ UserShell | sed 's/UserShell: //'`
 echo "DEFAULT SHELL: $default_shell"
 
 if [ "$default_shell" = "/bin/zsh" ]; then
-	echo "Identified ZHS as default shell. Exporting variables to ~/.zshrc"
-	printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.zshrc
+  echo "Identified ZHS as default shell. Exporting variables to ~/.zshrc"
+  printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.zshrc
 else
-	echo "Identified bash as default shell. Exporting variables to ~/.bashrc"
-	printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.bashrc
+  echo "Identified bash as default shell. Exporting variables to ~/.bashrc"
+  printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.bashrc
 fi
 
 DIR=$HOME/node
 if [ ! -d "$DIR" ] || [ ! "$(ls -A $DIR)" ]; then
   rm -rf $DIR
   
-	mkdir -p $DIR
-	cd $DIR
-	pwd
-	curl https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/cmd/updater/update.sh -O
-	ls -lt
-	chmod 544 update.sh
-	echo "AFTER CHMOD"
-	ls -lt
-	./update.sh -i -c stable -p $DIR -d $DIR/data -n
+  mkdir -p $DIR
+  cd $DIR
+  pwd
+  curl https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/cmd/updater/update.sh -O
+  ls -lt
+  chmod 544 update.sh
+  echo "AFTER CHMOD"
+  ls -lt
+  ./update.sh -i -c stable -p $DIR -d $DIR/data -n
 
-	export ALGORAND_DATA="$DIR/data"
-	export PATH="$DIR:$PATH"
+  export ALGORAND_DATA="$DIR/data"
+  export PATH="$DIR:$PATH"
 
-	goal node stop
-	goal node start
+  goal node stop
+  goal node start
 else
   echo "Error: ${DIR} with files found. Can not continue with installation."
   exit 1
--- linux/install.sh
+++ linux/install.sh
@@ -7,30 +7,30 @@ default_shell=$SHELL
 echo "DEFAULT SHELL: $SHELL"
 
 if [ "$default_shell" = "/bin/bash" ]; then
-	echo "Identified bash as default shell. Exporting variables to ~/.bashrc"
-	printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.bashrc
+  echo "Identified bash as default shell. Exporting variables to ~/.bashrc"
+  printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.bashrc
 fi
 
 DIR=$HOME/node
 if [ ! -d "$DIR" ] || [ ! "$(ls -A $DIR)" ]; then
-	rm -rf $DIR
+  rm -rf $DIR
 
-	mkdir -p $DIR
-	cd $DIR
-	pwd
-	wget https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/cmd/updater/update.sh
-	ls -lt
-	chmod 544 update.sh
-	echo "AFTER CHMOD"
-	lt -lt
-	./update.sh -i -c stable -p $DIR -d $DIR/data -n
+  mkdir -p $DIR
+  cd $DIR
+  pwd
+  wget https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/cmd/updater/update.sh
+  ls -lt
+  chmod 544 update.sh
+  echo "AFTER CHMOD"
+  lt -lt
+  ./update.sh -i -c stable -p $DIR -d $DIR/data -n
 
-	export ALGORAND_DATA="$DIR/data"
-	export PATH="$DIR:$PATH"
+  export ALGORAND_DATA="$DIR/data"
+  export PATH="$DIR:$PATH"
 
-	goal node stop
-	goal node start
+  goal node stop
+  goal node start
 else
-	echo "Error: ${DIR} with files found. Can not continue with installation."
+  echo "Error: ${DIR} with files found. Can not continue with installation."
   exit 1
 fi
