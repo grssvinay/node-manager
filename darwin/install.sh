a70aea58f8f61829c162090f30819ad84628a204
Vinay Garimella <vinay.garimella@NTPL-ITMACLAP0208.local>
2022-08-08 16:48:15 +0530

    in-process catchup

--- darwin/install.sh
+++ darwin/install.sh
@@ -1,3 +1,4 @@
+
 pwd
 chmod "u+x" ./darwin/start.sh
 chmod "u+x" ./darwin/stop.sh
@@ -9,20 +10,21 @@ if [ "$default_shell" = "/bin/zsh" ]; then
 	echo "Identified ZHS as default shell. Exporting variables to ~/.zshrc"
 	printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.zshrc
 else
-	echo "Identified ZHS as default shell. Exporting variables to ~/.zshrc"
+	echo "Identified bash as default shell. Exporting variables to ~/.bashrc"
 	printf "%s\n" "export ALGORAND_DATA=\"\$HOME/node/data\"" "export PATH=\"\$HOME/node:\$PATH\"" >> ~/.bashrc
 fi
 
 
-mkdir -p ~/node
-cd ~/node
+
+mkdir -p $HOME/node
+cd $HOME/node
 pwd
 curl https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/cmd/updater/update.sh -O
 ls -lt
 chmod 544 update.sh
 echo "AFTER CHMOD"
 ls -lt
-./update.sh -i -c stable -p ~/node -d ~/node/data -n
+./update.sh -i -c stable -p $HOME/node -d $HOME/node/data -n
 
 export ALGORAND_DATA="$HOME/node/data"
 export PATH="$HOME/node:$PATH"
--- linux/install.sh
+++ linux/install.sh
@@ -14,15 +14,15 @@ else
 fi
 
 
-mkdir -p ~/node
-cd ~/node
+mkdir -p $HOME/node
+cd $HOME/node
 pwd
 wget https://raw.githubusercontent.com/algorand/go-algorand/rel/stable/cmd/updater/update.sh
 ls -lt
 chmod 544 update.sh
 echo "AFTER CHMOD"
 lt -lt
-./update.sh -i -c stable -p ~/node -d ~/node/data -n
+./update.sh -i -c stable -p $HOME/node -d $HOME/node/data -n
 
 export ALGORAND_DATA="$HOME/node/data"
 export PATH="$HOME/node:$PATH"
