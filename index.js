4b7584abb6095162cb055dbfa5753213ee752ff9
Vinay Garimella <vinay.garimella@NTPL-ITMACLAP0208.local>
2022-08-08 16:48:15 +0530

    in-process catchup

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const app = express();
+
 var os = require('os');
 const { spawn } = require("child_process");
 
-const app = express();
+const { HTTP_STATUS_CODES, DEFAULT_PORT, HTTP_STATUS_MESSAGES }  = require('./utils/constants');
 
 const streamRoutes = require('./routes/v1/stream');
-const { HTTP_STATUS_CODES, DEFAULT_PORT, HTTP_STATUS_MESSAGES }  = require('./utils/constants');
-var processArgs = require('minimist')(process.argv.slice(2));
-
 app.use("/api/v1/stream", streamRoutes);
 
+var processArgs = require('minimist')(process.argv.slice(2));
 const PORT = processArgs['p'] || DEFAULT_PORT
 
 app.listen(PORT, (error) => {
@@ -20,7 +20,7 @@ app.listen(PORT, (error) => {
 
         cmd.on('error', (error) => console.log(`error: ${error.message}`));
         cmd.on("close", code => console.log(code == 0 ? `File ${fileName} access updated to executable successfully` : `child process closed with code ${code}`));
-        cmd.stderr.on("data", data => console.log(stderr));
+        cmd.stderr.on("data", data => console.log(data));
         cmd.stdout.on("data", data => console.log(data));
     } else {
         console.log("Error occurred, server can't start", error);
